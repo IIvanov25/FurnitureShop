4465112fca32f85cda25eb78dc653cab6d692fe2
Моника Атанасова <matanasova037@gmail.com>
2024-07-24 14:48:41 +0300

    Fixed OrdersService

--- FurnitureShopNew/FurnitureShopNew/Repositories/OrdersRepo.cs
+++ FurnitureShopNew/FurnitureShopNew/Repositories/OrdersRepo.cs
@@ -1,43 +1,46 @@
-﻿using FurnitureShopNew.Models;
+﻿using FurnitureShopNew;
+using FurnitureShopNew.Models;
+using FurnitureShopNew.Repositories;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 
-namespace FurnitureShopNew.Repositories
+public class OrdersRepo : IOrdersRepo
 {
-    public class OrdersRepo : IOrdersRepo
+    private readonly ShopDbContext _context;
+    public OrdersRepo(ShopDbContext context)
     {
-        private readonly ShopDbContext _context;
-        public OrdersRepo(ShopDbContext context)
-        {
-            _context = context;
-        }
-
-        void IOrdersRepo.AddOrders(Order orders)
-        {
-            _context.Orders.Add(orders);
-        }
-
-        void IOrdersRepo.DeleteOrders(int ordersId)
-        {
-            
-        }
-
-        IEnumerable<Order> IOrdersRepo.GetAllOrders()
-        {
-            throw new NotImplementedException();
-        }
-
-        List<Order> IOrdersRepo.GetAllOrdersByUser(User customer)
-        {
-            throw new NotImplementedException();
-        }
-
-        Order IOrdersRepo.GetOrderById(int orderId)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IOrdersRepo.UpdateOrders(Order order)
-        {
-            throw new NotImplementedException();
-        }
+        _context = context;
+    }
+    public void AddOrders(Order orders)
+    {
+        _context.Orders.Add(orders);
+        _context.SaveChanges();
+    }
+    public void DeleteOrders(int ordersId)
+    {
+        var orderToRemove = _context.Orders.First(o => o.OrderId == ordersId);
+        _context.Remove(orderToRemove);
+        _context.SaveChanges();
+    }
+    public IEnumerable<Order> GetAllOrders()
+    {
+        return _context.Orders.ToList();
+    }
+    public List<Order> GetAllOrdersByUser(User customer)
+    {
+        return _context.Orders.Where(o => o.UserId == customer.UserId).ToList();
+    }
+    public Order GetOrderById(int orderId)
+    {
+        return _context.Orders.Single(c => c.OrderId == orderId);
+    }
+    public void UpdateOrders(Order order)
+    {
+        _context.Orders.Update(order);
+        _context.SaveChanges();
     }
 }
