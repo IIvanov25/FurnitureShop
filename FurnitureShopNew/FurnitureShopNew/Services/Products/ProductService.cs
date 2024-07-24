e1bd5be8cfa73e60118161bb4a8140247171d15d
DanitoAnastasova <danianastasova2006@gmail.com>
2024-07-24 15:10:53 +0300

    Basic logic of ProductService

--- FurnitureShopNew/FurnitureShopNew/Services/Products/ProductService.cs
+++ FurnitureShopNew/FurnitureShopNew/Services/Products/ProductService.cs
@@ -1,6 +1,17 @@
 ﻿using FurnitureShopNew.Models;
+using FurnitureShopNew.Repositories;
+using FurnitureShopNew.Services;
 
 public class ProductService : IProductService
 {
-  
+ 
+    public Task<IEnumerable<Product>> GetAllProductsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetProductByIdAsync(int productId)
+    {
+        throw new NotImplementedException();
+    }
 }
