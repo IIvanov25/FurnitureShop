b9d2ef02e4c7bbc911d062e1eac298be773390bc
AusP3r1sh <ph03nixpr0t0k0l@gmail.com>
2024-07-25 12:41:49 +0300

    Added logic to CategoriesService and changed FurnitureTypeCategory to List<string>.

--- FurnitureShopNew/FurnitureShopNew/Models/Enums/FurnitureTypeCategory.cs
+++ FurnitureShopNew/FurnitureShopNew/Models/Enums/FurnitureTypeCategory.cs
@@ -1,19 +1,24 @@
 namespace FurnitureShopNew.Models
 {
-    public enum FurnitureTypeCategory
-    {
-        Chair = 0,
-        Table = 1,
-        Sofa = 2,
-        Bed = 3,
-        Cabinet = 4,
-        Desk = 5,
-        Wardrobe = 6,
-        Couch = 7,
-        Bookcase = 8,
-        BedsideTable = 9,
-        WallCabinet = 10,
-        WallShelves = 11,
-        Other = 12
-    }
+    private List<string> _categories;
+
+        public FurnitureTypeCategory()
+        {
+            _categories = new List<string>
+            {
+                "Chair",
+                "Table",
+                "Sofa",
+                "Bed",
+                "Cabinet",
+                "Desk",
+                "Wardrobe",
+                "Couch",
+                "Bookcase",
+                "BedsideTable",
+                "WallCabinet",
+                "WallShelves",
+                "Other"
+            };
+        }
 }
