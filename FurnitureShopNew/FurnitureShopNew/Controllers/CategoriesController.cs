72b194bdfa954636abdab62254fa9c0bafd04174
Alexander Shishkov <albertgen.relativity@gmail.com>
2024-07-25 17:22:18 +0300

    CategoryController

--- FurnitureShopNew/FurnitureShopNew/Controllers/CategoriesController.cs
+++ FurnitureShopNew/FurnitureShopNew/Controllers/CategoriesController.cs
@@ -1,22 +1,62 @@
 using FurnitureShopNew.Models;
 using FurnitureShopNew.Repositories;
+using FurnitureShopNew.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FurnitureShopNew.Controllers
 {
+    [Route("controller")]
+    [ApiController]
     public class CategoriesController : Controller
     {
-        private readonly FurnitureTypeCategory _categories;
-        private readonly CategoriesRepo _categoriesRepo;
+        private readonly ICategoriesService _categoriesService;
 
-        public CategoriesController()
+        public CategoriesController(ICategoriesService categoriesService)
         {
+            _categoriesService = categoriesService;
+        }
+        [HttpGet("getAllCategories")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            try
+            {
+                var categories = _categoriesService.GetAllCategories();
 
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+        [HttpGet("getCategoryById")]
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            try
+            {
+                var category = _categoriesService.GetCategoryById(id);
+
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+        [HttpGet("getCategoryByName")]
+        public async Task<IActionResult> GetCategoryByName(string name)
+        {
+            try
+            {
+                var category = _categoriesService.GetCategoryByName(name);
+
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
     }
 }
